4713d9ed294dd3daac0ac7d1c14f45dc5efba98c
Jiyun19 <jiyunlee219@gmail.com>
2022-01-14 09:08:58 +0900

    1 file modified

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-function Hello(props) {
+function Hello(props){
   return <p>Hello, {props.name}!</p>;
 }
 
-const el = <Hello name="David" />; 
+const el=<Hello name="David"/>;
 ReactDOM.render(
-  el, 
+  el,
   document.getElementById('root')
 );
